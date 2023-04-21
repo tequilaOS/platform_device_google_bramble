02b3bf55856bc47add042da8c12e7d90aa1948ba
x0x00044 <x0x00044@x0x00044.me>
2023-04-21 22:38:22 +0200

    bramble: Time for tequila!

--- tequila_bramble.mk
+++ tequila_bramble.mk
@@ -4,21 +4,22 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common tequila stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/tequila/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/bramble/aosp_bramble.mk)
 
-include device/google/bramble/device-lineage.mk
+include device/google/bramble/device-tequila.mk
 
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 4a (5G)
-PRODUCT_NAME := lineage_bramble
+PRODUCT_NAME := tequila_bramble
 
 # Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
 
